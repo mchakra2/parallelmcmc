12161241a39cd798d0945ac1db0e1661d079c366
Maghesree Chakraborty <mchakra2@ur.rochester.edu>
2016-12-14 00:22:13 -0500

    Added paralle code

--- mcmc/mcmc.py
+++ mcmc/mcmc.py
@@ -6,19 +6,24 @@ import graphviz as gz
 import numpy as np
 import os
 import timeit
-import joblib
-
+import multiprocessing
+import random
+#from joblib import Parallel, delayed #For parallelization
+from multiprocessing import Pool,Value
 class MarkovChain:
     input_f='./IOFiles/input.txt'#default input file location
     o_file="./IOFiles/output.txt"#default output file location
-
+    test=0
     G1=nx.Graph()
     G2=nx.Graph()
     iterations=200#Number of Steps in the simulation
     T=1
     r=1
     uniques={}#Empty dictionary to keep track of unique graphs and number of times they are observed
-    
+    exp_d0=0.0#Expectation of degree of vertex 0
+    exp_edgs=0.0#Expectation of number of edges
+    exp_max_path=0.0#Expection of the maximum value of minimum paths
+
     def main(self):
         start_time = timeit.default_timer()
         #Clear G1 and G2 in case they are not empty
@@ -27,22 +32,39 @@ class MarkovChain:
         self.input_arg(self.input_f)
         self.make_init_graph()
         self.uniques.clear()
-        self.mc_chain_generator()
+        self.exp_d0=0#Expectation of degree of vertex 0
+        self.exp_edgs=0
+        self.exp_max_path=0
+        np = multiprocessing.cpu_count()
+        print ('Number of CPUs to be used:  {0:1d}'.format(np))
+        process_iter=[int(self.iterations/np) for i in range(np)]#Number of interations for each processes
+        print(process_iter)
+        #Creating the worker pool
+        pool = Pool(processes=np)   
+        # parallel map
+        results=pool.map(self.mc_chain_generator, process_iter)#this is a list of tuples returned from the parallel operation
+        for i in range(len(results)):
+            self.exp_d0+=results[i][0]
+            self.exp_max_path+=results[i][1]
+            self.exp_edgs+=results[i][2]
+            #unique_graphs
+        print('test',self.test)
         #d0, E and avg_path are for calculating the expectations from the sum of these attributes over the period of simulation
-        d0=float(self.exp_d0)/self.iterations
-        E=float(self.exp_edgs)/self.iterations
-        avg_path=float(self.exp_max_path)/self.iterations
-        print('The expected number of edges connected to vertex 0 is ',d0)
-        print('The expected number of edges in the entire graph ',E)
-        print('The expected maximum distance of the shortest path in a graph that connects vertex 0 to another vertex',avg_path)
+        self.exp_d0=float(self.exp_d0)/self.iterations
+        self.exp_edgs=float(self.exp_edgs)/self.iterations
+        self.exp_max_path=float(self.exp_max_path)/self.iterations
+        print('The expected number of edges connected to vertex 0 is ',self.exp_d0)
+        print('The expected number of edges in the entire graph ',self.exp_edgs)
+        print('The expected maximum distance of the shortest path in a graph that connects vertex 0 to another vertex',self.exp_max_path)
         print('The number of unique graphs ',len(self.uniques))
-        self.quantiling()
+        #self.quantiling()
         elapsed = timeit.default_timer() - start_time
         print(elapsed)
-        return(d0,E,avg_path)
+        #return(self.exp_d0,E,avg_path)
     
-    '''Function to calculate the weight of an edge. The weight is the Euclidian distance between two node tuples'''
+   
     def dist(self,a,b):
+        '''Function to calculate the weight of an edge. The weight is the Euclidian distance between two node tuples'''
         if type(a)==tuple and type(b)==tuple:
             wt=math.sqrt((a[1]-b[1])**2+(a[0]-b[0])**2)
             return(wt)
@@ -51,8 +73,9 @@ class MarkovChain:
             print ("Inputs should be tuples")
             raise TypeError
 
-    '''Fuction for Reading the input file and storing the nodes as a list of tuples'''     
+        
     def input_arg(self, in_file):
+        '''Fuction for Reading the input file and storing the nodes as a list of tuples''' 
         self.M=[]#list for the node tuples
         if os.path.exists(in_file)!= True:
             print ("The input file path does not exist. Default values will be used")
@@ -82,37 +105,44 @@ class MarkovChain:
         f.close()
 
         
-    '''Function to  make the initial graph G1 with the given  nodes. I have just connected node 0 or the first node in M to all other nodes in M'''
+    
     def make_init_graph(self):
-        
-        self.G1.add_nodes_from(self.M)
+        '''Function to  make the initial graph G1 with the given  nodes. 
+        I have just connected node 0 or the first node in M to all other nodes in M'''
+        G1=nx.Graph()
+        G1.add_nodes_from(self.M)
         for i in range(1,len(self.M)):
             
-            self.G1.add_edge(self.M[0],self.M[i],weight=self.dist(self.M[0],self.M[i]))
+            G1.add_edge(self.M[0],self.M[i],weight=self.dist(self.M[0],self.M[i]))
+        return(G1)
        
-    '''Function to change the state of the edge between the given nodes. If the edge is not present, it is added else it is removed if it is not a bridge'''
-    def graph_change(self,idx1,idx2):
-        self.G2=deepcopy(self.G1)
+   
+    def graph_change(self,idx1,idx2,G1):
+        '''Function to change the state of the edge between the given nodes. 
+        If the edge is not present, it is added else it is removed if it is not a bridge'''
+
+        G2=deepcopy(G1)
         
         v1=self.M[idx1]
         v2=self.M[idx2]
         
-        if self.G2.has_edge(v1,v2)==False:#Edge between v1 and v2 is not present. Hence add the edge.
+        if G2.has_edge(v1,v2)==False:#Edge between v1 and v2 is not present. Hence add the edge.
             
-            self.G2.add_edge(v1,v2,weight=self.dist(v1,v2))
-            return(1)
+            G2.add_edge(v1,v2,weight=self.dist(v1,v2))
+            return(G2)
         
         else:
-            if len(nx.minimum_edge_cut(self.G2,v1,v2))==1:#Edge is present but it is a bridge. The graph is not altered 
+            if len(nx.minimum_edge_cut(G2,v1,v2))==1:#Edge is present but it is a bridge. The graph is not altered 
 
                 return(-1)
             
             else:
-                self.G2.remove_edge(v1,v2)#Edge is removed if it is present in graph but not a bridge
-                return(0)
+                G2.remove_edge(v1,v2)#Edge is removed if it is present in graph but not a bridge
+                return(G2)
 
-    #Function to calculate the number of bridges in a given graph
+    
     def calculate_bridges(self,G):
+        '''Function to calculate the number of bridges in a given graph'''
         if type(G)!=nx.Graph:
             print ("Argument passed to the function should be a Graph")
             raise TypeError
@@ -124,15 +154,17 @@ class MarkovChain:
                 b+=1
         return(b)
     
-    #Function to calculate the q(m|n)  probabilitie by taking in Xn as argument
+    
     def calculate_q(self,G):
+        '''Function to calculate the q(m|n)  probabilitie by taking in Xn as argument'''
         b=self.calculate_bridges(G)
         nodes=float(G.number_of_nodes())
         q=(nodes*(nodes-1)/2)-b
         return(1/q)
 
-    #Funtion to return theta(Xi)
+    
     def theta_func(self,G):
+        '''Funtion to return theta(Xi)'''
         theta=self.r*G.size(weight='weight')
         for i in range(1,G.number_of_nodes()):
             theta+=nx.shortest_path_length(G,source=G.nodes()[0],target=G.nodes()[i],weight='weight')
@@ -140,11 +172,11 @@ class MarkovChain:
         return(theta)
 
     #Function to implement metropolis-hastings algorithm
-    def MH(self):
+    def MH(self,G1,G2):
 
-        f=math.exp(-float(self.theta_func(self.G2)-self.theta_func(self.G1))/self.T)
+        f=math.exp(-float(self.theta_func(G2)-self.theta_func(G1))/self.T)
 
-        q=self.calculate_q(self.G2)/self.calculate_q(self.G1)
+        q=self.calculate_q(G2)/self.calculate_q(G1)
         aij=min(f*q,1)
         U=np.random.random()#randomly chosen number between 0 and 1
         if aij>=U: 
@@ -154,60 +186,64 @@ class MarkovChain:
 
     #Function to return the maximum of the shortest path from vertex 0 to other vertices
     def max_shortest_path(self,G):
-        #start_time = timeit.default_timer()
         P=nx.shortest_path_length(G, source=self.M[0],weight='weight')
         max_path=-1
         for i in P:
             max_path=max(max_path,P[i])
-        #elapsed = timeit.default_timer() - start_time
-        #print(elapsed)
+        #P=Parallel(n_jobs=2)(delayed(nx.shortest_path_length)(G,source=self.M[0],target=i,weight='weight') for i in self.M[1:])
+        #max_path=np.amax(P)
         return(max_path)
 
     #Function to count uniques
-    def graph_count(self,G):
+    def graph_count(self,G,uniques):
         key=frozenset(G.edges(nbunch=self.M))
 
-        if key in self.uniques:#increment count if G has been observed before
-            self.uniques[key]+=1
+        if key in uniques:#increment count if G has been observed before
+            uniques[key]+=1
         else:#add to the dictionary if G has not been observed before
-            self.uniques[key]=1
+            uniques[key]=1
+            
 
     #Function to generate the markov chain
-    def mc_chain_generator(self):
-        diff_g=0
-        self.uniques.clear()
-        self.exp_d0=0#Expectation of degree of vertex 0
-        self.exp_edgs=0
-        self.exp_max_path=0
-        for i in range(self.iterations):#Propose graph  modification at each simulation step
-            flag=-1
-            while(flag==-1):#if  the randomly selected edge is a bridge select a different edge
-
-                A=np.random.choice(len(self.M), 2,replace=0)#Choose a tuple randomly without replacement from the range of indices in M
-                flag=self.graph_change(A[0],A[1])#graph_change  function returns -1 if the edge  is  a bridge 
+    def mc_chain_generator(self,iterations):
+        unique_graphs={}
+        #uniques.clear()
+        self.test+=1
+        exp_d0=0#Expectation of degree of vertex 0
+        exp_edgs=0
+        exp_max_path=0
+        G1=self.make_init_graph()#initial graph
+        for i in range(iterations):#Propose graph  modification at each simulation step
+            G2=-1
+            while(G2==-1):#if  the randomly selected edge is a bridge select a different edge
+                A=random.sample(range(len(self.M)), 2)#Choose a tuple randomly without replacement from the range of indices in M
+                #A=np.random.choice(len(self.M), 2,replace=0)#Choose a tuple randomly without replacement from the range of indices in M
+                G2=self.graph_change(A[0],A[1],G1)#graph_change  function returns -1 if the edge  is  a bridge 
 
                             
-            accept=self.MH()
+            accept=self.MH(G1,G2)
             if accept==1:#Accept the change if MH function returns 1
-                
-                self.G1=deepcopy(self.G2)
+                #print(i,"accept")
+                G1=deepcopy(G2)
 
             #Maintaining running averages for performing statistical analysis later
-            self.exp_d0+=self.G1.degree(self.M[0])
-            self.exp_edgs+=self.G1.number_of_edges()
-            self.exp_max_path+=self.max_shortest_path(self.G1)
-            self.graph_count(self.G1)
-        self.G1.clear()
-        self.G2.clear()
-        print(len(self.uniques))
+            exp_d0+=G1.degree(self.M[0])
+            exp_edgs+=G1.number_of_edges()
+            exp_max_path+=self.max_shortest_path(G1)
+            self.graph_count(G1,unique_graphs)
+        
+        return(exp_d0,exp_max_path,exp_edgs,unique_graphs)
+        
 
-    #Function to return a list of edge-list of top 1% graphs generated in the markov chain
-    def quantiling(self):
+
+    def quantiling(self,dictionary):
+        '''Function to take in a dictionary of unique graphs and their occurances.
+        It returns a list of edge-list of top 1% graphs generated in the markov chain'''
         print('The edge lists of the top 1% graphs are printed in ', self.o_file)
         f=open(self.o_file,"w")#Writing into the output file
         
-        desc_adj=sorted(self.uniques.items(), key=itemgetter(1), reverse=True)
-        top=0.01*len(self.uniques)
+        desc_adj=sorted(dictionary.items(), key=itemgetter(1), reverse=True)
+        top=0.01*len(dictionary)
         top_graphs=[]
         if top<1:
             print('Since there are less than 100 unique graphs, only the most likely graph will be written in the output file')
@@ -222,3 +258,11 @@ class MarkovChain:
         
         f.close()
         return(top_graphs)
+
+if __name__ == '__main__':
+    m=MarkovChain()
+    m.main()
+
+
+
+   
--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -13,13 +13,14 @@ import sys
 import os
 import unittest
 import networkx as nx
+import random
 from contextlib import contextmanager
 from click.testing import CliRunner
 from mcmc import mcmc
 from copy import deepcopy
 from operator import itemgetter
-
-
+from multiprocessing import Pool
+import multiprocessing
 class TestMcmc(unittest.TestCase):
 
     def setUp(self):
@@ -60,48 +61,61 @@ class TestMcmc(unittest.TestCase):
         self.assertGreater(len(self.m.M),0)#To ensure that the number of vertices is greater than 0
 
     def test_make_init_graph(self):
-        self.m.G1.clear()
+        #self.m.G1.clear()
         self.m.input_arg('./tests/test_input.txt')
-        self.m.make_init_graph()
-        self.assertIsInstance(self.m.G1,nx.Graph)
-        self.assertTrue(nx.is_connected(self.m.G1),'Initial Graph is connected')#checks if initial graph is connected
+        G1=self.m.make_init_graph()
+        self.assertIsInstance(G1,nx.Graph)
+        self.assertTrue(nx.is_connected(G1),'Initial Graph is connected')#checks if initial graph is connected
         
 
 
-
+    
     def test_graph_change_add(self):
+        '''Checks if graph_change function adds an edge 
+        if it is not already present between the selected nodes'''
         self.m.input_arg('./tests/test_input.txt')
-        self.m.make_init_graph()
-        self.assertEqual(self.m.graph_change(1,2),1)#Since no two non zero vertices are connected, passing two vertices other than vertex0 sould add an edge
-        self.assertNotEqual(self.m.G1.number_of_edges(),self.m.G2.number_of_edges())#ensures that the new graph is not the same as the old graph
-        self.m.G1.clear()
-        self.m.G2.clear()
+        G1=self.m.make_init_graph()
+        G2=self.m.graph_change(1,2,G1)
+        flag=G2.number_of_edges()-G1.number_of_edges()
+        self.assertEqual(flag,1)#Since no two non zero vertices are connected, passing two vertices other than vertex0 sould add an edge
+        self.assertTrue(nx.is_connected(G2))#ensures that the new graph is connected
+        #self.m.G1.clear()
+        #self.m.G2.clear()
 
 
     def test_graph_change_bridge(self):
+        '''Checks if graph_change function keeps the graph
+        unchanged a bridge is present between the selected nodes'''
         self.m.input_arg('./tests/test_input.txt')
-        self.m.make_init_graph()
-        self.assertEqual(self.m.graph_change(0,2),-1)
-        self.assertEqual(self.m.G1.number_of_edges(),self.m.G2.number_of_edges())
+        G1=self.m.make_init_graph()
+        self.assertEqual(self.m.graph_change(0,2,G1),-1)
+        #self.assertEqual(self.m.G1.number_of_edges(),self.m.G2.number_of_edges())
         self.m.G1.clear()
-        self.m.G2.clear()
+        #self.m.G2.clear()
 
 
     def test_graph_change_remove(self):
+        '''Checks if graph_change function the edge 
+        present between the selected nodes provided it is not a bridge'''
         self.m.input_arg('./tests/test_input.txt')
-        self.m.make_init_graph()
+        G1=self.m.make_init_graph()
         v1=self.m.M[1]
         v2=self.m.M[2]
         wt=self.m.dist(v1,v2)
-        print(wt)
-        self.m.G1.add_edge(v1,v2,weight=wt)
-        self.assertEqual(self.m.graph_change(1,2),0)
-        self.assertNotEqual(self.m.G1.number_of_edges(),self.m.G2.number_of_edges())
-        self.m.G1.clear()
-        self.m.G2.clear()
+        #print(wt)
+        G1.add_edge(v1,v2,weight=wt)
+        G2=self.m.graph_change(1,2,G1)
+        flag=G2.number_of_edges()-G1.number_of_edges()
+        self.assertEqual(flag,-1)#to ensure that an edge has been removed
+        self.assertTrue(nx.is_connected(G2))#ensures that the new graph is connected
+        #self.m.G1.clear()
+        #self.m.G2.clear()
 
 
     def test_calculate_bridges(self):
+        '''This tests if the calculate_bridges function
+        can calculate the number of bridges in a provided 
+        graph correctly'''
         a=1
         self.assertRaises(TypeError,self.m.calculate_bridges,a)
         G=nx.cycle_graph(3)
@@ -119,18 +133,18 @@ class TestMcmc(unittest.TestCase):
         G1.clear()
 
     def test_main(self):
-        
-        deg,edge,path=self.m.main()
-        
+        '''Tests the sanity of the return values of the main function'''
+        self.m.main()
         #self.assertEqual(len(self.m.uniques),4)
         nodes=len(self.m.M)
         max_edges=nodes*(nodes-1)/2
-        self.assertGreater(edge,nodes-1)#number of edges should be more than M-1 to stay connected. M is the number of nodes
-        self.assertLessEqual(edge,max_edges)#number of edges should be less than or equal to M(M-1)/2 where M is the number of nodes
-        self.assertGreater(deg,0)#to check that at least one or mode edges are connected to node 0
-        self.assertLessEqual(deg,nodes-1)#deg of vertex 0 should not me more than M-1
+        self.assertGreater(self.m.exp_edgs,nodes-1)#number of edges should be more than M-1 to stay connected. M is the number of nodes
+        self.assertLessEqual(self.m.exp_edgs,max_edges)#number of edges should be less than or equal to M(M-1)/2 where M is the number of nodes
+        self.assertGreater(self.m.exp_d0,0)#to check that at least one or more edges are connected to node 0
+        self.assertLessEqual(self.m.exp_d0,nodes-1)#deg of vertex 0 should not me more than M-1
         #self.m.G1.clear()
         #self.m.G2.clear()
+
 
     #Theta value should be greater than 0
     def test_theta_func(self):
@@ -140,67 +154,65 @@ class TestMcmc(unittest.TestCase):
 
     def test_MH(self):
         self.m.input_arg('./tests/test_MH.txt')
-        self.m.make_init_graph()
-        self.m.G2=deepcopy(self.m.G1)
+        G1=self.m.make_init_graph()
+        G2=deepcopy(G1)
         v1=self.m.M[1]
         v2=self.m.M[2]
-        self.m.G1.add_edge(v1,v2,weight=self.m.dist(v1,v2))
-        print(self.m.G1.number_of_edges())
-        print(self.m.G2.number_of_edges())
-        self.assertEqual(self.m.MH(),1)
-        self.m.G1.clear()
-        self.m.G2.clear()
+        G1.add_edge(v1,v2,weight=self.m.dist(v1,v2))
+        #print(self.m.G1.number_of_edges())
+        #print(G2.number_of_edges())
+        self.assertEqual(self.m.MH(G1,G2),1)
+        #self.m.G1.clear()
+        #self.m.G2.clear()
 
     #The input file has only three nodes. And the initial graph has two edges connecting node 0 to the other two nodes. The longer edge is of length 2
     def test_max_shortest_path(self):
-        self.m.G1.clear()
+        #self.m.G1.clear()
         self.m.input_arg('./tests/test_MH.txt')
-        self.m.make_init_graph()
-        self.assertEqual(self.m.max_shortest_path(self.m.G1),2)
+        G1=self.m.make_init_graph()
+        self.assertEqual(self.m.max_shortest_path(G1),2)
         
     #To check if graph counter actually keeps track of unique graph
     def test_graph_counter_behavior(self):
         self.m.input_arg('./tests/test_MH.txt')
-        self.m.make_init_graph()
-        self.assertEqual(len(self.m.uniques),0)
-        self.m.graph_count(self.m.G1)
-        self.assertEqual(len(self.m.uniques),1)
-        flag=self.m.graph_change(1,2)
-        self.m.graph_count(self.m.G2)
-        self.assertEqual(len(self.m.uniques),2)
-        self.m.graph_count(self.m.G1)
-        self.assertEqual(len(self.m.uniques),2)
-        key=frozenset(self.m.G1.edges(nbunch=self.m.M))
-        self.assertEqual(self.m.uniques[key],2)
+        G1=self.m.make_init_graph()
+        uniques={}
+        self.assertEqual(len(uniques),0)
+        self.m.graph_count(G1,uniques)
+        self.assertEqual(len(uniques),1)
+        G2=self.m.graph_change(1,2,G1)
+        self.m.graph_count(G2,uniques)
+        self.assertEqual(len(uniques),2)
+        self.m.graph_count(G1,uniques)
+        self.assertEqual(len(uniques),2)
+        key=frozenset(G1.edges(nbunch=self.m.M))
+        self.assertEqual(uniques[key],2)
         self.m.uniques.clear()
-        self.m.G1.clear()
-        self.m.G2.clear()
+        #self.m.G1.clear()
+        #self.m.G2.clear()
 
     #Total count of graphs in the markov chain should be equal to the number of iterations. To check that.
     def test_total_graph_count(self):
         self.m.input_arg('./tests/test_input.txt')
         self.m.make_init_graph()
-        self.m.mc_chain_generator()
-        self.assertEqual(self.m.iterations,sum(self.m.uniques.values()))
+        exp_d0,exp_edgs,exp_max_path,uniques=self.m.mc_chain_generator(self.m.iterations)
+        self.m.mc_chain_generator(self.m.iterations)
+        self.assertEqual(self.m.iterations,sum(uniques.values()))
         self.m.uniques.clear()
         
     #Our test_MH.txt has only three nodes hence there are only 4 different connected graphs possible. This checks that the number of uniques after simulation is not more than the maximum possible unique graphs
     def test_mc_chain_generator(self):
         self.m.input_arg('./tests/test_MH.txt')
-        self.m.make_init_graph()
-        self.m.mc_chain_generator()
-        assert (len(self.m.uniques)>0 and len(self.m.uniques)<=4)
-        self.m.G1.clear()
-        self.m.G2.clear()
+        exp_d0,exp_edgs,exp_max_path,uniques=self.m.mc_chain_generator(self.m.iterations)
+        assert (len(uniques)>0 and len(uniques)<=4)
+        #self.m.G1.clear()
+        #self.m.G2.clear()
         
     #Testing the quantiling function when the number of unique graphs is less than 100. Since top 1% is fractional, the single most likely graph is returned 
     def test_quantiling_single(self):
-        self.m.G1.clear()
-        self.m.G2.clear() 
         self.m.input_arg('./tests/test_MH.txt')
-        self.m.make_init_graph()
-        self.m.mc_chain_generator()
-        top=len(self.m.quantiling())
+        exp_d0,exp_edgs,exp_max_path,uniques=self.m.mc_chain_generator(self.m.iterations)
+        top=len(self.m.quantiling(uniques))
         self.assertEqual(top,1)
         #To check that the output file is not empty
         flag = os.path.exists(self.m.o_file)
@@ -209,14 +221,11 @@ class TestMcmc(unittest.TestCase):
     
     #Testing the quantiling function when the number of unique graphs is greater than or equal 100
     def test_quantiling_multiple(self):
-        self.m.G1.clear()
-        self.m.G2.clear() 
         self.m.input_arg('./tests/test_input.txt')
-        self.m.make_init_graph()
-        self.m.mc_chain_generator()
-        top=len(self.m.quantiling())
-        print(top,(0.01*len(self.m.uniques)))
-        assert (top-round(0.01*len(self.m.uniques)))<0.0001
+        exp_d0,exp_edgs,exp_max_path,uniques=self.m.mc_chain_generator(self.m.iterations)
+        top=len(self.m.quantiling(uniques))
+        print(top,(0.01*len(uniques)))
+        assert (top-round(0.01*len(uniques)))<0.0001
         #To check that the output file is not empty
         flag = os.path.exists(self.m.o_file)
         self.assertTrue(flag)
